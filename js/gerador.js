868193e6d7d584f584bb69375b50c2cdda3d4570
todydanielm3 <todydanielm3@github.com>
2022-05-25 04:05:39 -0300

    Prazo/Validade/Visualizações

--- js/gerador.js
+++ js/gerador.js
@@ -1,6 +1,7 @@
 let senhaElemento = document.querySelector("div.gerador-senha p")
 const botaoGerarSenha = document.querySelector("div.gerador-senha button") 
 const botaoValidade  = document.querySelector("div.validade button")
+
 const caracteres = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","w","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","W","Y","0","1","2","3","4","5","6","7","8","9","@","#","$"]
 //const botaourl = document.querySelector("div.url button")
 
@@ -30,7 +31,7 @@ botaoGerarSenha.onclick = ()=>{
 
 }
 botaoValidade.onclick = ()=>{
-  window.location = "../urlsenha.html"
+  window.location = "urlsenha.html"
 
 }
 
